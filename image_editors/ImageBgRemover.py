4b016ce354663132ccd17ead374a1606b7530416
josewebdev2000 <josewebdev@proton.me>
2023-07-10 20:05:49 -0400

    Began to Build Image Bg Remover

---
+++ image_editors/ImageBgRemover.py
@@ -0,0 +1,6 @@
+"""
+    This file contains an Image Background Remover class to handle image background removing operations.
+"""
+
+class ImageBgRemover(object):
+    """Handle Image Background Removing."""
